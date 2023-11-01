22c4ff8c700568b7002f49735752284741b4c50c
ashsic <99445200+ashsic@users.noreply.github.com>
2023-11-01 09:17:19 -0400

    Added arrow function syntax (faster)

--- 01_day_JS.js
+++ 01_day_JS.js
@@ -6,9 +6,18 @@ var createHelloWorld = function() {
     return function(...args) {
         return "Hello World";
     };
+
+    // Or this (faster)
+    // var createHelloWorld = function() {
+    //     return () => "Hello World";
+    // };
+
 };
 
-/**
+
+
+
+/** test case
  * const f = createHelloWorld();
  * f(); // "Hello World"
  */
